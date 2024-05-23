0fe6d8dc5d588a5451b26254534463d9a940aa54
Mabel-LL <mleonl5@upao.edu.pe>
2024-05-22 21:28:12 -0500

    tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicarSesionComponent } from './inicar-sesion/inicar-sesion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicarSesionComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/inicar-sesion/inicar-sesion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicar-sesion',
+  templateUrl: './inicar-sesion.component.html',
+  styleUrls: ['./inicar-sesion.component.css']
+})
+export class InicarSesionComponent {
+
+}
