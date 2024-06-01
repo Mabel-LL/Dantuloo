5700a39f2d0ade076e3457c50c28334a6376f314
Mabel-LL <mleonl5@upao.edu.pe>
2024-05-31 22:51:48 -0500

    DAN-03 Cambiar contraseña

--- src/app/components/layout/navbar-intranet/navbar-intranet.component.ts
+++ src/app/components/layout/navbar-intranet/navbar-intranet.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar-intranet.component.css']
 })
 export class NavbarIntranetComponent {
+  public showModal: boolean = false;
+  public notifications: string[] = [
+    'Tu solicitud para el viaje de Trujillo a Lima a las 3:00pm fue aceptada.',
+    'Tu solicitud para el viaje de Trujillo a Lima a las 3:00pm fue aceptada.',
+    'Tu solicitud para el viaje de Trujillo a Lima a las 3:00pm fue aceptada.',
+    'Tu solicitud para el viaje de Trujillo a Lima a las 3:00pm fue aceptada.'
+  ];
 
+  openModal(): void {
+    this.showModal = true;
+  }
+
+  closeModal(): void {
+    this.showModal = false;
+  }
+
+  deleteNotifications(): void {
+    this.notifications = [];
+  }
 }
