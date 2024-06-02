9e35abb5b8e21f361549aba5861efb2d8b9c866d
Mabel-LL <mleonl5@upao.edu.pe>
2024-06-01 19:57:18 -0500

    DAN-11 validaciones

--- src/app/components/autenticacion/iniciar-sesion/iniciar-sesion.component.spec.ts
+++ src/app/components/autenticacion/iniciar-sesion/iniciar-sesion.component.spec.ts
@@ -1,21 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IniciarSesionComponent } from './iniciar-sesion.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('IniciarSesionComponent', () => {
   let component: IniciarSesionComponent;
   let fixture: ComponentFixture<IniciarSesionComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [IniciarSesionComponent],
+      imports: [ReactiveFormsModule]
+    })
+      .compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [IniciarSesionComponent]
-    });
     fixture = TestBed.createComponent(IniciarSesionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('deberia crear', () => {
+  it('debería crear', () => {
     expect(component).toBeTruthy();
   });
 });
