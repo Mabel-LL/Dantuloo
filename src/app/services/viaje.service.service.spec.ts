9d650ce1d9b25f671a5515c3462b631af5659586
Angelo Sanchez <jsanchezs17@upao.edu.pe>
2024-06-09 23:08:13 -0500

    DAN-13 - Integracion de Publicar viaje y pequeño cambio de autenticacion en usuario.service.ts

--- src/app/services/viaje.service.service.spec.ts
+++ src/app/services/viaje.service.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ViajeService } from './viaje.service';
+import {HttpClientModule} from "@angular/common/http";
 
 describe('ViajeServiceService', () => {
   let service: ViajeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(ViajeService);
   });
 
